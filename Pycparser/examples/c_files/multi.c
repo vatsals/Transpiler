ce90594f110c96a97941ce0fec63e5ff1c867830
vatsals <vatsalshrivastav26@gmail.com>
2018-01-29 02:22:27 +0530

    PycParser added

---
+++ Pycparser/examples/c_files/multi.c
@@ -0,0 +1,15 @@
+int main() {
+	int a=1, b;
+	b = a*b;
+	printf("The answer is: %d",b);
+
+	if(a==b) {
+		printf("The answer is: %d",c);
+	}
+
+	// while(1) {
+	// 	printf("The answer is: %d",a);
+	// }
+
+	return 0;
+}
